bfe64ca9c9c93226ae09814f1a2d616b8f3e94a5
Gabriel_GN <86376459+Gabriel-gn21@users.noreply.github.com>
2025-03-27 13:54:05 -0300

    Inserção do cronômetro e finalização do projeto

--- scripts.js
+++ scripts.js
@@ -4,10 +4,17 @@ const elemVoltar = document.querySelector(".voltar");
 const elemClose = document.querySelector(".close");
 const elemPlay = document.querySelector(".play");
 const elemPause = document.querySelector(".pause");
-const elemCronometro = document.querySelector(".cronometro")
-const elemHorario = document.querySelector(".horario")
+const elemCronometro = document.querySelector(".cronometro");
+const elemHorario = document.querySelector(".horario");
+const elemMinuto = document.querySelector(".minuto");
+const elemSegundo = document.querySelector(".segundo");
+const elemCentesimo = document.querySelector(".centesimo");
+
+var timer = 0;
+var idIntervalo = null;
 
 // Adicionar classe tela--cronometro
+
 elemsExercicio.forEach(function (elemExercicio) {
         elemExercicio.addEventListener("click", function () {
             elemTela.classList.add("tela--cronometro");
@@ -16,26 +23,41 @@ elemsExercicio.forEach(function (elemExercicio) {
 
 elemVoltar.addEventListener("click", function() {
     elemTela.classList.remove("tela--cronometro");
-    //console.log("Clicou!")
+
+    clicouClose();
 });
 
-
-elemClose.addEventListener("click", function() {
+function clicouClose() {
     elemCronometro.classList.remove("iniciou");
-    //console.log("Clicou!")
+    clearInterval(idIntervalo);
+    timer = 0;
+    atualizarCronometro();
+}
+elemClose.addEventListener("click", function() {
+    clicouClose();
 });
+
+function clicouPlay () {
+    elemCronometro.classList.add("iniciou");
+
+    rodarTimer()
+}
 
 elemPlay.addEventListener("click", function() {
-    elemCronometro.classList.add("iniciou");
-    //console.log("Clicou!")
-});
+    clicouPlay();
+}
+)
+
+function clicouPause () {
+    elemCronometro.classList.remove("iniciou");
+    clearInterval(idIntervalo);
+}
 
 elemPause.addEventListener("click", function() {
-    elemCronometro.classList.remove("iniciou");
-    //console.log("Clicou!")
+    clicouPause();
 });
 
-// Atualizar horario
+// Atualiza o horario
 
 function atualizarHorario() {
     const horas = new Date().getHours().toString().padStart(2,"0");;
@@ -43,7 +65,6 @@ function atualizarHorario() {
     const horario = horas + ":" + minutos;
 
     elemHorario.innerText= horario;
-    //console.log(horario);
 }
 
 atualizarHorario();
@@ -51,3 +72,33 @@ atualizarHorario();
 setInterval(() => {
     atualizarHorario();
 }, 1000);
+
+// Construindo o cronometro
+
+function rodarTimer() {
+    setInterval(() => {
+        timer = timer + 10;
+        console.log(timer);
+    }, 10);
+}
+
+function rodarTimer() {
+    idIntervalo = setInterval(() => {
+        timer = timer + 10;
+        console.log(timer);
+        atualizarCronometro();
+    }, 10);
+}
+
+function atualizarCronometro() {
+    const minutos = Math.floor(timer / 60000).toString().padStart(2,"0");
+    const segundos = Math.floor((timer % 60000) / 1000).toString().padStart(2, "0");
+    const centesimos = Math.floor(((timer % 6000) % 1000) / 10).toString().padStart(2, "0");
+    const tempoCronometro = minutos + ":" + segundos + "," + centesimos;
+
+    elemMinuto.innerText = minutos;
+    elemSegundo.innerText = segundos;
+    elemCentesimo.innerText = centesimos;
+
+    console.log(tempoCronometro);
+}
